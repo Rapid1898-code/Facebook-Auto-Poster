7c314093def7540d1e607153a7fd9e977379817a
Rapid1898 <markuspolzer@gmx.at>
2021-02-20 16:21:13 +0100

    created repo

---
+++ FacebookAutomaticPost.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import sys, os
+from sys import platform
+import xlwings as xw
+from datetime import datetime, timedelta
+
+def login(id,password):
+    email = driver.find_element_by_id("email")
+    email.send_keys(id)
+    Password = driver.find_element_by_id("pass")
+    Password.send_keys(password)
+    button = driver.find_element_by_name("login").click()
+    pass
+
+# read input excel
+FN = "FacebookAutomaticPost.xlsx"
+path = os.path.abspath (os.path.dirname (sys.argv[0]))
+fn = path + "/" + FN
+wb = xw.Book (fn)
+ws = wb.sheets["Overview"]
+ws2 = wb.sheets["Text"]
+listGroups = ws.range ("B5:B100").value
+for idx, cont in enumerate (listGroups):
+  if cont == None:
+    maxRow = int (idx)
+    break
+idxRow = 2
+
+# login to facebook
+link = "https://www.facebook.com/"
+options = Options()
+options.add_argument("--disable-infobars")
+options.add_argument("start-maximized")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-notifications")
+options.add_argument("--disable-application-cache")
+if ws["B3"].value.upper() not in ["YES","JA","J","Y"]:
+    options.add_argument('--headless')
+options.add_experimental_option ('excludeSwitches', ['enable-logging'])
+path = os.path.abspath (os.path.dirname (sys.argv[0]))
+if platform == "win32": cd = '/chromedriver.exe'
+elif platform == "linux": cd = '/chromedriver_linux'
+elif platform == "darwin": cd = '/chromedriver'
+driver = webdriver.Chrome (path + cd, options=options)
+driver.get (link)  # Read link
+time.sleep (3)
+# driver.find_element_by_xpath ("//*[@id="u_0_j_Zi"]").click ()
+driver.find_element_by_css_selector("[title*='Alle akzeptieren']").click()
+print(f"Press on <Accept All> button...")
+time.sleep (3)
+login(ws["B1"].value,ws["B2"].value )
+print(f"Login To Facebook...")
+time.sleep (3)
+
+for idx, stock in enumerate (listGroups):
+    if idx == maxRow:
+        break
+    linkcode = ws["B" + str (idxRow)].value
+    textCell = ws["D" + str (idxRow)].value
+    dateCell = ws["E" + str (idxRow)].value
+    # print("Debug: ", linkcode,textCell,dateCell)
+
+    if textCell not in [None,""] and dateCell in [None,""]:
+        text = ws2[textCell].value
+    else:
+        idxRow += 1
+        continue
+
+    dt1 = datetime.today ()
+    dt1 = datetime.strftime (dt1, "%Y-%m-%d")
+
+    if dateCell in [None,""]:
+        try:
+            link2 = "https://www.facebook.com/groups" + linkcode
+            driver.get (link2)
+            print(f"Working on link {link2}...")
+            time.sleep (3)
+            driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span').click()
+            print(f"Click in Message field...")
+            time.sleep (3)
+            driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div/div/div/div')\
+            .send_keys(text)
+            print(f"Write text to message box...")
+            time.sleep (3)
+            driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[2]/div/div/div[1]').click()
+            idxRow += 1
+            print(f"Send message...")
+            time.sleep (10)
+            ws["E" + str (idxRow-1)].value = dt1
+            print(f"Message send on Facebook for row {idxRow} site {linkcode} for cell {textCell}")
+        except Exception as e:
+            print(f"Error while working on row {idxRow} for site {linkcode} - wrote error to column E...")
+            print(f"ErrorCode: {e}")
+            ws["E" + str (idxRow - 1)].value = "Error"
+            idxRow += 1
+driver.quit()
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Facebook-Auto-Poster
-Facebook Automatic Posting with Python
+With this Program you can post automatic messages on Facebook Groups which you can parameterize in an excel sheet. 
+
+Have a look at my general Online-FAQ for my tools:  
+https://www.rapidtech1898.com/htmlFinanztools/en/faqFinanceToolsEN.html
+  
+## First Steps / Pay Attention
+* Please use it with caution and don´t make any spams on Facebook an their groups - Thanks!  
+* Please check whether the chromedriver version matches your currently installed Chrome version - the appropriate chromedriver version can be downloaded here (https://chromedriver.chromium.org/downloads) and put it in the same folder of the program
+* The firewall access may be activated the first time the program is started (click on "OK" or "Allow") - The program pulls data through online scrapping and
+therefore needs online access rights
+* It is best not to edit the Excel sheet during the update - otherwise, in rare cases this can lead to conflicts
+* Make and save settings in the Excel sheet - then start the program - the Excel will be updated
+
+
+## Input Fields in FacebookAutomaticPost.xlsx
+* All input fields are generally in yellow  
+* Cell B1: Your Facebook-User for login
+* Cell B2: Your Facebook-Password for login (Caution! Please be aware where to put your excel-sheet...)
+* Cell B3: If yes/y/ja/j => You can see the working on the windows during program run - otherwise it will run in the background
+* Column B (starting Cell B5): the link name of the Facebook group  
+e.g. for the Python group on Facebook the full link is: https://www.facebook.com/groups/python  
+so the relevant link for these entry in column B would be "/python"
+* Column D (starting Cell D5): the text-cell which should be posted in the Facebook-group according to the "Text"-worksheet entries
+
+
+## Program workflow
+* Make your inputs in the excel-sheet an save
+* Let the program run...  
+* If this post worked without problems - the actual date will be placed in the corresponding cell in column E  
+* If there are any errors during the work on a post (e.g. some additional approvement window for the 
+first post) then there will be written "error" in the corresponding cell in column E  
+(if the error is only due the approval of the first post - the second run should be fine - so delete the error message and try again)
+
+Finally in case of problems or questions have a look at my general Online-FAQ for my tools:  
+https://www.rapidtech1898.com/htmlFinanztools/en/faqFinanceToolsEN.html
+
+If this doesn´t help pls feel free to get in contanct with me!
+
